c05e73a025baaac232360c2ddee2a440ff1253e5
asmodi17 <asmodi17@gmail.com>
2013-11-12 11:02:11 -0700

    Added glyph memory map
    Including the glyph codes in fake ASCII.txt
    Updated vid.mem to include glyph memory map
    VGA2 now displays the glyph memory map.  I'm going to set up a method
    for writing to this memory on the fly so we can display values for the
    current instruction, the registers, memory addresses, memory data (not
    video memory).  This will likely require separating the glyph memory
    block from the glyph map memory block.

--- VGA2.v
+++ VGA2.v
@@ -19,7 +19,11 @@ module VGA2( CLK, CLR, RGB_in, HPix, VPix, RGB_out, HSync, VSync );
 	reg [1:0] clkCount;
 	reg pixCLK;
 	
-	pixelGen2 _pixelGen( pixCLK, CLR, maxH, maxV, HPix, VPix, RGB_out );
+	wire [11:0] addr1, addr2;
+	
+	
+	//pixelGen2 _pixelGen( CLK, CLR, maxH, maxV, HPix, VPix, RGB_out );
+	pixelGen3 _pixelGen( CLK, CLR, maxH, maxV, HPix, VPix, RGB_out, addr1, addr2, d_out1, d_out2 );
 	
 	always @ (posedge CLK) begin
 		if (CLR) begin pixCLK = 1'b0; clkCount = 2'b0; end
@@ -34,28 +38,107 @@ module VGA2( CLK, CLR, RGB_in, HPix, VPix, RGB_out, HSync, VSync );
 
 endmodule
 
+// This module displays the entire alphabet on each line
 module pixelGen2( CLK, CLR, maxH, maxV, HPix, VPix, RGB_out, addr1, d_out1 );
 	input CLK, CLR;
 	input [9:0] HPix, VPix;
 	input [9:0] maxH, maxV;
 	output reg [7:0] RGB_out;
 
-	output [9:0] addr1;
-	wire [9:0] addr2;
+	output [11:0] addr1;
+	wire [11:0] addr2;
 	wire [17:0] d_in1, d_in2, d_out2;
 	output [17:0] d_out1;
 	
 	vidMemory vid_mem(CLK, w1, addr1, d_in1, d_out1, CLK, w2, addr2, d_in2, d_out2);
 	
-	assign addr1 = (HPix == 0 || VPix == 0) ? 10'b0000000000 : 4*((HPix - 1)/8) + (((VPix - 1) % 8)/2);
-	//assign RGB_out = (HPix == 0 || VPix == 0) ? 8'b00000000 : (d_out1[2 + ((HPix  - 1) % 8 + (((VPix - 1) % 2) * 8))] == 1'b1) ? 8'b11111111 : 8'b00000000;
+	assign addr1 = (HPix == 0 || VPix == 0) ? 12'b0000000000 : 4*((HPix - 1)/8) + (((VPix - 1) % 8)/2);
 	
-	/*
-	Assume the addresses are correct
-	Assume VPix = 1
-	HPix 1-8 
+	always @ (*) begin
+		if (HPix == 0 || VPix == 0) begin RGB_out = 8'b00000000; end 
+		else begin
+			if (d_out1[17 - (2 + ((HPix - 1) % 8 + (((VPix - 1) % 2) * 8)))] == 1) begin
+				RGB_out = 8'b11111111;
+			end
+			else begin
+				RGB_out = 8'b00000000;
+			end
+		end
+	end
 	
-	*/
+endmodule
+
+// This module remembers a set of glyphs for each line.
+// 80 glyphs per row, 27 memory addresses*3 = 81, but only 80
+// can be displayed.  27 memory addresses*60 total lines = 1620 memory
+// addresses for the full screen.
+// 4096 memory addresses for this video memory
+// 164 for the alphabet as written, so if we start at address 256,
+// we stop at 1875
+module pixelGen3( CLK, CLR, maxH, maxV, HPix, VPix, RGB_out, addr1, addr2, d_out1, d_out2 );
+	input CLK, CLR;
+	input [9:0] HPix, VPix;
+	input [9:0] maxH, maxV;
+	output reg [7:0] RGB_out;
+
+	output [11:0] addr2;
+	output reg [11:0] addr1;
+	//wire [11:0] addr2;
+	wire [17:0] d_in1, d_in2;
+	output [17:0] d_out1, d_out2;
+	
+	wire w1, w2;
+	assign w1 = 0;
+	assign w2 = 0;
+	assign d_in1 = 0;
+	assign d_in2 = 0;
+	
+	vidMemory vid_mem(CLK, w1, addr1, d_in1, d_out1, CLK, w2, addr2, d_in2, d_out2);
+	
+	//assign addr1 = (HPix == 0 || VPix == 0) ? 12'b0000000000 : 4*((HPix - 1)/8) + (((VPix - 1) % 8)/2);
+	
+	// Find a way to set the correct address from the glyph map
+	// so HPix 1 VPix 1 starts us with memory address 256
+	
+	// addr2 will be used to pull glyph row information from d_out2.  d_out2 will
+	// be used to generate addr1 for all glyph pixel information to generate
+	// RGB_out from d_out1
+	
+	// So, addr2 can be calculated by finding (HPix - 1) / 24 to get the first and second VPix rows,
+	// but VPix will also have to be divided.  (VPix - 1) / 8 will give us the correct row, but we have to
+	// multiple the result of that by 27 since there are 27 glyphs per row.
+	// Once we add in our starting address, we should be good to go.
+	
+	//assign addr2 = 9'd256 + (HPix - 1'b1)/(5'd24) + ((VPix - 1'b1)/2)*(5'd27);
+	assign addr2 = 256 + (HPix - 1)/24 + ((VPix - 1)/8)*27;	
+	
+	// Now that addr2 has been set correctly, we need to get addr1 from d_out2
+	// VPix has already been taken into account and ensures that we are on the
+	// correct row, but we need to find out which character we are displaying.
+	// (HPix - 1) % 24 will give us a number from 0-23 and depending on the
+	// result, we can obtain the correct pixel.  Since glyphs consist of 6bits,
+	// HPix 0-7 will correspond to the top 6 bits of the 18-bit data, or 17-12
+	// HPix 8-15 will correspond to the second 6 bits of the 18-bit data, or 11-6
+	// HPix 16-23 will correspond to the last 6 bits of the 18-bit data, or 5-0
+	
+	// Need to take into account VPix.  2 rows per address, 4 addresses per glyph.
+	// Remember to multiply the glyph code by 4 (since each glyph takes up
+	// 4 addresses.
+	
+	always @ (*) begin
+		if ((HPix - 1) % 24 >= 16) begin
+			// case for the last 6 bits of data
+			addr1 = (d_out2[5:0])*4 + ((VPix - 1) % 8)/2 ;
+		end
+		else if ((HPix - 1) % 24 >= 8) begin
+			// case for the middle 6 bits of data
+			addr1 = (d_out2[11:6])*4 + ((VPix - 1) % 8)/2 ;
+		end
+		else begin
+			// case for the first 6 bits of data
+			addr1 = (d_out2[17:12])*4 + ((VPix - 1) % 8)/2 ;
+		end
+	end
 	
 	always @ (*) begin
 		if (HPix == 0 || VPix == 0) begin RGB_out = 8'b00000000; end 
@@ -77,7 +160,7 @@ module pixelGen( CLK, CLR, maxH, maxV, HPix, VPix, RGB_out );
 	input [9:0] maxH, maxV;
 	output reg [7:0] RGB_out;
 		
-	//memory vid_mem(CLK, w1, addr1, d_in1, d_out1, w2, addr2, d_in2, d_out2);
+	//vidMemory vid_mem(CLK, w1, addr1, d_in1, d_out1, w2, addr2, d_in2, d_out2);
 	
 	wire [1:0] x;
 	assign x = (HPix/32 + VPix/32) % 4;
