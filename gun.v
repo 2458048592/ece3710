a82466551a46ff2e9b835645526cd5a7ea136f26
Dan Willoughby <amozoss@gmail.com>
2013-11-15 14:34:53 -0700

    updating comments

--- gun.v
+++ gun.v
@@ -26,15 +26,16 @@ module gun(
 );
 
 	// First use two flip-flops to synchronize the trigger signal the "clk" clock domain
-	reg trigger_sync_0; always @(posedge clk) trigger_sync_0 <= trigger;  // invert trigger to make trigger_sync_0 active high
+	reg trigger_sync_0; always @(posedge clk) trigger_sync_0 <= trigger;  
 	reg trigger_sync_1; always @(posedge clk) trigger_sync_1 <= trigger_sync_0;
 
 	// Next declare a 16-bits counter
 	reg [15:0] trigger_cnt;
 	reg trigger_state; // 1 as long as the push-button is active (down)
-	// When the push-button is pushed or released, we increment the counter
+	
+	
+	// When the trigger is pushed or released, we increment the counter
 	// The counter has to be maxed out before we decide that the push-button state has changed
-
 	wire trigger_idle = (trigger_state==trigger_sync_1);
 	wire trigger_cnt_max = &trigger_cnt;	// true when all bits of trigger_cnt are 1's
 
@@ -50,7 +51,6 @@ module gun(
 		else begin
 			 trigger_cnt <= trigger_cnt + 16'd1;  // something's going on, increment the counter
 			 if(trigger_cnt_max) begin
-				//trigger_cnt <= trigger_cnt;
 				trigger_state <= ~trigger_state;  // if the counter is maxed out, trigger changed!
 			end
 		end
