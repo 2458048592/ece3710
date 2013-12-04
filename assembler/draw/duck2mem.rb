0bc0896445b35d64317dc786a922d8818e094a54
Dan Willoughby <amozoss@gmail.com>
2013-12-03 22:32:21 -0700

    drawing 32 and 64 pixel duck

--- assembler/draw/duck2mem.rb
+++ assembler/draw/duck2mem.rb
@@ -5,21 +5,33 @@ pixels = []
 image = Magick::Image.read("duck64.png")[0]
 image.format = 'bgra'
 #blob = image.to_blob
+new_pixel = red = green = blue = alpha = 0
 
+count = 0
 image.each_pixel do |pixel, c, r|
   #red=65535, green=65535, blue=65535, opacity=65021
 
   pixels.push(pixel)
-  print "#{pixel}\n"
 
   pixel = pixel.to_s
   pixel =~ /^\bred\b=(\d+).\s+\bgreen\b=(\d+).\s+\bblue\b=(\d+).\s+\bopacity\b=(\d+)$/
-  red = $1
-  green = $2
-  blue = $3
-  alpha = $4
+  red   = 0b01000 if $1.to_i > 20000
+  green = 0b00100 if $2.to_i > 20000
+  blue  = 0b00010 if $3.to_i > 20000
+  alpha = 0b00001 if $4.to_i > 0
+  #print "r:#{red} g:#{green} b:#{blue} o:#{alpha}\n"
+  new_pixel += red
+  new_pixel += green
+  new_pixel += blue
+  new_pixel += alpha
+  #print "#{pixel}\n"
+  printf "0" if count % 4 == 0 
+  printf "%01x", new_pixel
+  printf "\n" if count % 4 == 3 
+  count += 1
+  new_pixel = red = green = blue = alpha = 0
     
-  print "r:#{red} g:#{green} b:#{blue} o:#{alpha}\n"
+  #print "r:#{red} g:#{green} b:#{blue} o:#{alpha}\n"
   
 end
 
---
+++ assembler/draw/readme.txt
@@ -0,0 +1,7 @@
+# For Ubuntu install
+
+# install libmagick9-dev
+sudo apt-get install libmagick9-dev
+
+# run bundle install to install the RMagick gem
+bundle install
